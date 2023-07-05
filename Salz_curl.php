d6733b94125186a9753b21295aa169a802a83d8b
Mochammad Faisal <faisal_animator@yahoo.com>
2023-07-05 13:56:45 +0700

    Major Changes
    Add :
    - Disable SSL Verif
    - Response Header Callback
    
    Update :
    - Http Code
    - Encode Json
    - Multipart Body

--- Salz_curl.php
+++ Salz_curl.php
@@ -2,11 +2,11 @@
 
 /*
  *
- * Salz Library Curl v1.2.0
+ * Salz Library Curl v1.6.3
  *
  * Created by Mochammad Faisal
  * Created at 2020-12-10 11:46:25
- * Updated at 2023-04-13 09:27:32
+ * Updated at 2023-07-05 13:53:54
  *
  */
 
@@ -20,7 +20,10 @@ class Salz_curl {
     protected $encode_json = false;
     protected $is_multipart_body = false;
     protected $is_get_http_code = false;
+    protected $is_disable_ssl_verif = false;
+    protected $is_header_response_enable = false;
     protected $res_http_code;
+    protected $res_headers = [];
 
     // get set
     public function get_Host() {
@@ -51,12 +54,20 @@ class Salz_curl {
         $this->body = $body;
     }
 
-    public function is_encode_json() {
+    public function get_encode_json() {
         return $this->encode_json;
     }
 
     public function set_encode_json($is_set) {
         $this->encode_json = $is_set;
+    }
+
+    public function get_is_multipart_body() {
+        return $this->is_multipart_body;
+    }
+
+    public function set_is_multipart_body($is_set) {
+        $this->is_multipart_body = $is_set;
     }
 
     public function get_res_http_code() {
@@ -73,6 +84,22 @@ class Salz_curl {
 
     public function set_is_http_code($is_set) {
         $this->is_get_http_code = $is_set;
+    }
+
+    public function set_is_disable_ssl_verif($is_set) {
+        $this->is_disable_ssl_verif = $is_set;
+    }
+
+    public function set_is_header_response_enable($is_set) {
+        $this->is_header_response_enable = $is_set;
+    }
+
+    public function get_is_header_response_enable() {
+        return $this->is_header_response_enable;
+    }
+
+    public function get_response_header() {
+        return $this->res_headers;
     }
 
     public function build_curl() {
@@ -109,18 +136,17 @@ class Salz_curl {
         if (!empty($this->body)) {
             $body = $this->body;
 
-            if ($this->is_encode_json()) {
+            if ($this->get_encode_json()) {
                 $body = json_encode($body);
             }
 
-            if ($this->is_multipart_body) {
+            if ($this->get_is_multipart_body()) {
                 $body = http_build_query($body);
             }
         }
 
         $headers = $headers_list;
-
-        curl_setopt_array($curl, array(
+        $options = array(
             CURLOPT_URL => $this->host,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
@@ -131,18 +157,30 @@ class Salz_curl {
             CURLOPT_CUSTOMREQUEST => $this->method,
             CURLOPT_HTTPHEADER => $headers,
             CURLOPT_POSTFIELDS => $body,
-        ));
+        );
+
+        if ($this->is_disable_ssl_verif) {
+            $options[CURLOPT_SSL_VERIFYHOST] = false;
+            $options[CURLOPT_SSL_VERIFYPEER] = false;
+        }
+
+        if ($this->get_is_header_response_enable()) {
+            // reff : https://github.com/stripe/stripe-php/issues/220
+            $options[CURLOPT_HEADERFUNCTION] = array($this, 'customHeaderCallback');
+        }
+
+        curl_setopt_array($curl, $options);
 
 
         // print_r($headers); exit;
         // print_r($body); exit;
         // die($body);
+        // print_r($options);exit;
 
         $response = curl_exec($curl);
         $err = curl_error($curl);
 
-
-        if ($this->is_get_http_code) {
+        if ($this->get_is_http_code()) {
             $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
             $this->set_res_http_code($http_status);
         }
@@ -154,12 +192,24 @@ class Salz_curl {
         // print_r($response);exit();
         // print_r($body);exit();
         // echo $response; exit;
+        // echo $err; exit;
 
         if ($err) {
             return json_decode($err, true);
         } else {
             return json_decode($response, true);
         }
+    }
+
+    public function customHeaderCallback($curl, $header_line) {
+        // reff : https://stackoverflow.com/a/51661183/10351006
+        // https://github.com/stripe/stripe-php/issues/220
+        if (strpos($header_line, ":") === false) {
+            return strlen($header_line);
+        }
+        list($key, $value) = explode(":", trim($header_line), 2);
+        $this->res_headers[trim($key)] = trim($value);
+        return strlen($header_line);
     }
 
     // end of class
